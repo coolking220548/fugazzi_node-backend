082c49079b6388627db6ca94f2b42b4a409b7eb7
coolking220548 <brandonjnrmark57@gmail.com>
2025-09-19 13:13:00 +1000

    Updates

--- index.js
+++ index.js
@@ -1,31 +1,128 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
+import multer from "multer";
 
 const app = express();
-const ML_API_URL = "https://fugazzi-flask-backend.onrender.com";
+const PYTHON_API_URL = "https://fugazzi-flask-backend.onrender.com/";
 const PORT = process.env.PORT || 5001;
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/api/analyze", async (req, res) => {
-  try {
-    const { text } = req.body;
+app.post("/api/newsAnalyze", async (req, res) => {
+	try {
+		const { text } = req.body;
 
-    // Forward to FastAPI ML backend
-    const response = await axios.post(ML_API_URL+"/detect", { text });
+		// Forward to FastAPI ML backend
+		const response = await axios.post(`${PYTHON_API_URL}news/predict`, { text });
 
-    res.json({
-      original: text,
-      prediction: response.data
-    });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ error: "ML service unavailable" });
-  }
+		res.json({
+			original: text,
+			prediction: response.data
+		});
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({ error: "ML service unavailable" });
+	}
 });
 
+app.post("/api/newsScrape", async (req, res) => {
+	try {
+		const {url} = req.body;
+
+		// Forward to FastAPI scraping backend
+		const response = await axios.post(`${PYTHON_API_URL}news/scrape`, {url});
+
+		res.json({
+			original: url,
+			articleText: response.data
+		});
+	} catch (error){
+		console.error(error.message);
+		res.status(500).json({ error: "Scraping service unavailable" });
+	}
+});
+
+app.post("/api/newsTextExtraction", upload.single("image"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({error: "No image uploaded"})
+        }
+
+        const base64image = req.file.buffer.toString("base64");
+
+        const response = await axios.post(`${PYTHON_API_URL}news/textExtract`, {
+            image: base64image,
+        });
+
+        res.json({
+            original: req.file.originalname,
+            classification: response.data
+        })
+    }catch (error){
+		console.error(error.message);
+		res.status(500).json({error: "Text Extraction service unavailable"});
+    }
+});
+
+app.post("/api/urlAnalyze", async (req, res) => {
+	try {
+		const {url} = req.body;
+
+		const response = await axios.post(`${PYTHON_API_URL}/url/predict`, {url});
+
+		res.json({
+			original: url,
+			classification: response.data
+		});
+	} catch (error){
+		console.error(error.message);
+		res.status(500).json({error: "URL authentication service unavailable"});
+	}
+});
+
+app.post("/api/fbAnalyze", async (req, res) => {
+	try {
+		const {url} = req.body;
+
+		const response = await axios.post(`${PYTHON_API_URL}fb/analyze`, {url});
+
+		res.json({
+			original: url,
+			classification: response.data
+		});
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).json({error: "Facebook Account Authentication service unavailable"});
+	}
+});
+
+app.post("/api/imageAnalyze", upload.single("image"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({error: "No image uploaded"})
+        }
+
+        const base64image = req.file.buffer.toString("base64");
+
+        const response = await axios.post(`${PYTHON_API_URL}image/analyze`, {
+            image: base64image,
+        });
+
+        res.json({
+            original: req.file.originalname,
+            classification: response.data
+        })
+    }catch (error){
+		console.error(error.message);
+		res.status(500).json({error: "Image Authentication service unavailable"});
+    }
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Node backend running on port ${PORT}`);
+  	console.log(`Node backend running on port ${PORT}`);
 });
